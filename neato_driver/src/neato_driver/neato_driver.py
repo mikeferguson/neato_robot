8cd63ce8e12a1bd8d93af28088b29aa2178125fa
Michael Ferguson <mfergs7@gmail.com>
2013-02-02 12:18:17 -0800

    import and catkinize, robot needs to charge in order to test

---
+++ neato_driver/src/neato_driver/neato_driver.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python
+
+# Generic driver for the Neato XV-11 Robot Vacuum
+# Copyright (c) 2010 University at Albany. All right reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the University at Albany nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL VANADIUM LABS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+neato_driver.py is a generic driver for the Neato XV-11 Robotic Vacuum.
+ROS Bindings can be found in the neato_node package.
+"""
+
+__author__ = "ferguson@cs.albany.edu (Michael Ferguson)"
+
+import serial
+
+BASE_WIDTH = 248    # millimeters
+MAX_SPEED = 300     # millimeters/second
+
+xv11_analog_sensors = [ "WallSensorInMM",
+                "BatteryVoltageInmV",
+                "LeftDropInMM",
+                "RightDropInMM",
+                "RightMagSensor",
+                "LeftMagSensor",
+                "XTemp0InC",
+                "XTemp1InC",
+                "VacuumCurrentInmA",
+                "ChargeVoltInmV",
+                "NotConnected1",
+                "BatteryTemp1InC",
+                "NotConnected2",
+                "CurrentInmA",
+                "NotConnected3",
+                "BatteryTemp0InC" ]
+
+xv11_digital_sensors = [ "SNSR_DC_JACK_CONNECT",
+                "SNSR_DUSTBIN_IS_IN",
+                "SNSR_LEFT_WHEEL_EXTENDED",
+                "SNSR_RIGHT_WHEEL_EXTENDED",
+                "LSIDEBIT",
+                "LFRONTBIT",
+                "RSIDEBIT",
+                "RFRONTBIT" ]
+
+xv11_motor_info = [ "Brush_MaxPWM",
+                "Brush_PWM",
+                "Brush_mVolts",
+                "Brush_Encoder",
+                "Brush_RPM",
+                "Vacuum_MaxPWM",
+                "Vacuum_PWM",
+                "Vacuum_CurrentInMA",
+                "Vacuum_Encoder",
+                "Vacuum_RPM",
+                "LeftWheel_MaxPWM",
+                "LeftWheel_PWM",
+                "LeftWheel_mVolts",
+                "LeftWheel_Encoder",
+                "LeftWheel_PositionInMM",
+                "LeftWheel_RPM",
+                "RightWheel_MaxPWM",
+                "RightWheel_PWM",
+                "RightWheel_mVolts",
+                "RightWheel_Encoder",
+                "RightWheel_PositionInMM",
+                "RightWheel_RPM",
+                "Laser_MaxPWM",
+                "Laser_PWM",
+                "Laser_mVolts",
+                "Laser_Encoder",
+                "Laser_RPM",
+                "Charger_MaxPWM",
+                "Charger_PWM",
+                "Charger_mAH" ]
+
+xv11_charger_info = [ "FuelPercent",
+                "BatteryOverTemp",
+                "ChargingActive",
+                "ChargingEnabled",
+                "ConfidentOnFuel",
+                "OnReservedFuel",
+                "EmptyFuel",
+                "BatteryFailure",
+                "ExtPwrPresent",
+                "ThermistorPresent[0]",
+                "ThermistorPresent[1]",
+                "BattTempCAvg[0]",
+                "BattTempCAvg[1]",
+                "VBattV",
+                "VExtV",
+                "Charger_mAH",
+                "MaxPWM" ]
+
+class xv11():
+
+    def __init__(self, port="/dev/ttyUSB0"):
+        self.port = serial.Serial(port,115200)
+        # Storage for motor and sensor information
+        self.state = {"LeftWheel_PositionInMM": 0, "RightWheel_PositionInMM": 0}
+        self.stop_state = True
+        # turn things on
+        self.setTestMode("on")
+        self.setLDS("on")
+
+    def exit(self):
+        self.setLDS("off")
+        self.setTestMode("off")
+
+    def setTestMode(self, value):
+        """ Turn test mode on/off. """
+        self.port.write("testmode " + value + "\n")
+
+    def setLDS(self, value):
+        self.port.write("setldsrotation " + value + "\n")
+
+    def requestScan(self):
+        """ Ask neato for an array of scan reads. """
+        self.port.flushInput()
+        self.port.write("getldsscan\n")
+
+    def getScanRanges(self):
+        """ Read values of a scan -- call requestScan first! """
+        ranges = list()
+        angle = 0
+        try:
+            line = self.port.readline()
+        except:
+            return []
+        while line.split(",")[0] != "AngleInDegrees":
+            try:
+                line = self.port.readline()
+            except:
+                return []
+        while angle < 360:
+            try:
+                vals = self.port.readline()
+            except:
+                pass
+            vals = vals.split(",")
+            #print angle, vals
+            try:
+                a = int(vals[0])
+                r = int(vals[1])
+                ranges.append(r/1000.0)
+            except:
+                ranges.append(0)
+            angle += 1
+        return ranges
+
+    def setMotors(self, l, r, s):
+        """ Set motors, distance left & right + speed """
+        #This is a work-around for a bug in the Neato API. The bug is that the
+        #robot won't stop instantly if a 0-velocity command is sent - the robot
+        #could continue moving for up to a second. To work around this bug, the
+        #first time a 0-velocity is sent in, a velocity of 1,1,1 is sent. Then, 
+        #the zero is sent. This effectively causes the robot to stop instantly.
+        if (int(l) == 0 and int(r) == 0 and int(s) == 0):
+            if (not self.stop_state):
+                self.stop_state = True
+            l = 1
+            r = 1
+            s = 1
+        else:
+            self.stop_state = False
+
+        self.port.write("setmotor "+str(int(l))+" "+str(int(r))+" "+str(int(s))+"\n")
+
+    def getMotors(self):
+        """ Update values for motors in the self.state dictionary.
+            Returns current left, right encoder values. """
+        self.port.flushInput()
+        self.port.write("getmotors\n")
+        line = self.port.readline()
+        while line.split(",")[0] != "Parameter":
+            try:
+                line = self.port.readline()
+            except:
+                return [0,0]
+        for i in range(len(xv11_motor_info)):
+            try:
+                values = self.port.readline().split(",")
+                self.state[values[0]] = int(values[1])
+            except:
+                pass
+        return [self.state["LeftWheel_PositionInMM"],self.state["RightWheel_PositionInMM"]]
+
+    def getAnalogSensors(self):
+        """ Update values for analog sensors in the self.state dictionary. """
+        self.port.write("getanalogsensors\n")
+        line = self.port.readline()
+        while line.split(",")[0] != "SensorName":
+            try:
+                line = self.port.readline()
+            except:
+                return
+        for i in range(len(xv11_analog_sensors)):
+            try:
+                values = self.port.readline().split(",")
+                self.state[values[0]] = int(values[1])
+            except:
+                pass
+
+    def getDigitalSensors(self):
+        """ Update values for digital sensors in the self.state dictionary. """
+        self.port.write("getdigitalsensors\n")
+        line = self.port.readline()
+        while line.split(",")[0] != "Digital Sensor Name":
+            try:
+                line = self.port.readline()
+            except:
+                return
+        for i in range(len(xv11_digital_sensors)):
+            try:
+                values = self.port.readline().split(",")
+                self.state[values[0]] = int(values[1])
+            except:
+                pass
+
+    def getCharger(self):
+        """ Update values for charger/battery related info in self.state dictionary. """
+        self.port.write("getcharger\n")
+        line = self.port.readline()
+        while line.split(",")[0] != "Label":
+            line = self.port.readline()
+        for i in range(len(xv11_charger_info)):
+            values = self.port.readline().split(",")
+            try:
+                self.state[values[0]] = int(values[1])
+            except:
+                pass
+
+    def setBacklight(self, value):
+        if value > 0:
+            self.port.write("setled backlighton")
+        else:
+            self.port.write("setled backlightoff")
+
+    #SetLED - Sets the specified LED to on,off,blink, or dim. (TestMode Only)
+    #BacklightOn - LCD Backlight On  (mutually exclusive of BacklightOff)
+    #BacklightOff - LCD Backlight Off (mutually exclusive of BacklightOn)
+    #ButtonAmber - Start Button Amber (mutually exclusive of other Button options)
+    #ButtonGreen - Start Button Green (mutually exclusive of other Button options)
+    #LEDRed - Start Red LED (mutually exclusive of other Button options)
+    #LEDGreen - Start Green LED (mutually exclusive of other Button options)
+    #ButtonAmberDim - Start Button Amber Dim (mutually exclusive of other Button options)
+    #ButtonGreenDim - Start Button Green Dim (mutually exclusive of other Button options)
+    #ButtonOff - Start Button Off
+
---
+++ neato_node/nodes/neato.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+# ROS node for the Neato Robot Vacuum
+# Copyright (c) 2010 University at Albany. All right reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the University at Albany nor the names of its 
+#       contributors may be used to endorse or promote products derived 
+#       from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL VANADIUM LABS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+ROS node for Neato XV-11 Robot Vacuum.
+"""
+
+__author__ = "ferguson@cs.albany.edu (Michael Ferguson)"
+
+import roslib; roslib.load_manifest("neato_node")
+import rospy
+from math import sin,cos
+
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf.broadcaster import TransformBroadcaster
+
+from neato_driver import xv11, BASE_WIDTH, MAX_SPEED
+
+class NeatoNode:
+
+    def __init__(self):
+        """ Start up connection to the Neato Robot. """
+        rospy.init_node('neato')
+
+        self.port = rospy.get_param('~port', "/dev/ttyUSB0")
+        rospy.loginfo("Using port: %s"%(self.port))
+
+        self.robot = xv11(self.port)
+
+        rospy.Subscriber("cmd_vel", Twist, self.cmdVelCb)
+        self.scanPub = rospy.Publisher('base_scan', LaserScan)
+        self.odomPub = rospy.Publisher('odom',Odometry)
+        self.odomBroadcaster = TransformBroadcaster()
+
+        self.cmd_vel = [0,0] 
+
+    def spin(self):        
+        encoders = [0,0]
+
+        self.x = 0                  # position in xy plane
+        self.y = 0
+        self.th = 0
+        then = rospy.Time.now()
+
+        # things that don't ever change
+        scan_link = rospy.get_param('~frame_id','base_laser_link')
+        scan = LaserScan(header=rospy.Header(frame_id=scan_link)) 
+        scan.angle_min = 0
+        scan.angle_max = 6.26
+        scan.angle_increment = 0.017437326
+        scan.range_min = 0.020
+        scan.range_max = 5.0
+        odom = Odometry(header=rospy.Header(frame_id="odom"), child_frame_id='base_link')
+    
+        # main loop of driver
+        r = rospy.Rate(5)
+        self.robot.requestScan()
+        while not rospy.is_shutdown():
+            # prepare laser scan
+            scan.header.stamp = rospy.Time.now()    
+            #self.robot.requestScan()
+            scan.ranges = self.robot.getScanRanges()
+
+            # get motor encoder values
+            left, right = self.robot.getMotors()
+
+            # send updated movement commands
+            self.robot.setMotors(self.cmd_vel[0], self.cmd_vel[1], max(abs(self.cmd_vel[0]),abs(self.cmd_vel[1])))
+            
+            # ask for the next scan while we finish processing stuff
+            self.robot.requestScan()
+            
+            # now update position information
+            dt = (scan.header.stamp - then).to_sec()
+            then = scan.header.stamp
+
+            d_left = (left - encoders[0])/1000.0
+            d_right = (right - encoders[1])/1000.0
+            encoders = [left, right]
+            
+            dx = (d_left+d_right)/2
+            dth = (d_right-d_left)/(BASE_WIDTH/1000.0)
+
+            x = cos(dth)*dx
+            y = -sin(dth)*dx
+            self.x += cos(self.th)*x - sin(self.th)*y
+            self.y += sin(self.th)*x + cos(self.th)*y
+            self.th += dth
+
+            # prepare tf from base_link to odom
+            quaternion = Quaternion()
+            quaternion.z = sin(self.th/2.0)
+            quaternion.w = cos(self.th/2.0)
+
+            # prepare odometry
+            odom.header.stamp = rospy.Time.now()
+            odom.pose.pose.position.x = self.x
+            odom.pose.pose.position.y = self.y
+            odom.pose.pose.position.z = 0
+            odom.pose.pose.orientation = quaternion
+            odom.twist.twist.linear.x = dx/dt
+            odom.twist.twist.angular.z = dth/dt
+
+            # publish everything
+            self.odomBroadcaster.sendTransform( (self.x, self.y, 0), (quaternion.x, quaternion.y, quaternion.z, quaternion.w),
+                then, "base_link", "odom" )
+            self.scanPub.publish(scan)
+            self.odomPub.publish(odom)
+
+            # wait, then do it again
+            r.sleep()
+
+        # shut down
+        self.robot.setLDS("off")
+        self.robot.setTestMode("off") 
+
+    def cmdVelCb(self,req):
+        x = req.linear.x * 1000
+        th = req.angular.z * (BASE_WIDTH/2) 
+        k = max(abs(x-th),abs(x+th))
+        # sending commands higher than max speed will fail
+        if k > MAX_SPEED:
+            x = x*MAX_SPEED/k; th = th*MAX_SPEED/k
+        self.cmd_vel = [ int(x-th) , int(x+th) ]
+
+if __name__ == "__main__":    
+    robot = NeatoNode()
+    robot.spin()
+
